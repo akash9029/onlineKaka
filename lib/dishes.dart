9d23d08f450bcf0f21ee02450752266d20a3c748
Akash <akash.verma639531@gmail.com>
2021-08-27 17:43:29 +0530

    mapped the list with widgets

--- lib/dishes.dart
+++ lib/dishes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:onlinekaka/about.dart';
 import 'package:onlinekaka/contact.dart';
 import 'package:onlinekaka/description.dart';
+import 'restaurant.dart';
 
 class DishesHome extends StatefulWidget {
   const DishesHome({Key? key}) : super(key: key);
@@ -11,7 +12,78 @@ class DishesHome extends StatefulWidget {
 }
 
 class _DishesHomeState extends State<DishesHome> {
-  List foodItems = ["", "", "", "", "", "", "", ""];
+  List<RestaurantName> restaurant = [
+    RestaurantName(
+        image: "assets/alamgir.jpg",
+        restaurantName: "Alamgir Hotel",
+        description: "Mughlai, Kebabs, Biryani",
+        location: "Naaz Cinema Road,Aminabad Lucknow UP 226018",
+        rating: "4.5",
+        activate: "open"),
+    RestaurantName(
+        image: "assets/awadhi.jpg",
+        restaurantName: "Awadhi Kakori Kabab (City Station)",
+        description: "Mughlai, Biryani",
+        location: "City Station, Wazirganj Lucknow UP 226022",
+        rating: "2.0",
+        activate: "open"),
+    RestaurantName(
+        image: "assets/alamgir.jpg",
+        restaurantName: "Alamgir Hotel",
+        description: "Mughlai, Kebabs, Biryani",
+        location: "Naaz Cinema Road,Aminabad Lucknow UP 226018",
+        rating: "4.5",
+        activate: "open"),
+    RestaurantName(
+        image: "assets/awadhi.jpg",
+        restaurantName: "Awadhi Kakori Kabab (City Station)",
+        description: "Mughlai, Biryani",
+        location: "City Station, Wazirganj Lucknow UP 226022",
+        rating: "2.0",
+        activate: "open"),
+    RestaurantName(
+        image: "assets/celebration.jpg",
+        restaurantName: "Celebrations By Onlinekaka",
+        description: "Flowers, Cakes",
+        location: "Hazratganj Lucknow Uttar Pradesh 226001",
+        rating: "5.0",
+        activate: "close"),
+    RestaurantName(
+        image: "assets/LaPinzo.png",
+        restaurantName: "La Pino'z Pizza (Hazratganj)",
+        description: "Pizza, Italian",
+        location: "Sapru Marg Lucknow UP 226001",
+        rating: "4.1",
+        activate: "open"),
+    RestaurantName(
+        image: "assets/Mubeens.jpeg",
+        restaurantName: "Mubeens",
+        description: "Mughlai, Biryani, Kulcha Nehari",
+        location: "Akbari Gate,Chowk Lucknow UP 226003",
+        rating: "3.0",
+        activate: "close"),
+    RestaurantName(
+        image: "assets/Shabaan.png",
+        restaurantName: "Shabaan Tikka Biryani",
+        description: "Mughlai, Biryani",
+        location: "Chowk Lucknow UP 226003",
+        rating: "5.0",
+        activate: "close"),
+    RestaurantName(
+        image: "assets/Subway.jpg",
+        restaurantName: "Subway (Hazratganj)",
+        description: "Healthy Food, Fast Food",
+        location: "6,Sapru Marg,Hazratganj Lucknow UP 226010",
+        rating: "4.0",
+        activate: "open"),
+    RestaurantName(
+        image: "assets/Tunday.png",
+        restaurantName: "Tunday Kababi (Aliganj)",
+        description: "Vegetarian, Mughlai, Kebabs, Biryani",
+        location: " Kapoorthala,Aliganj,Opp Novelty Cinema Lucknow UP 226024",
+        rating: "3.4",
+        activate: "open"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -122,64 +194,123 @@ class _DishesHomeState extends State<DishesHome> {
           child: Padding(
             padding: const EdgeInsets.only(top: 10),
             child: ListView(
-              children: [
-                createList(
-                    "assets/alamgir.jpg",
-                    "Alamgir Hotel",
-                    "Mughlai, Kebabs, Biryani",
-                    "Naaz Cinema Road,Aminabad Lucknow UP 226018",
-                    "4.5",
-                    "open"),
-                createList(
-                    "assets/awadhi.jpg",
-                    "Awadhi Kakori Kabab (City Station)",
-                    "Mughlai, Biryani",
-                    "City Station, Wazirganj Lucknow UP 226022",
-                    "2.0",
-                    "open"),
-                createList(
-                    "assets/celebration.jpg",
-                    "Celebrations By Onlinekaka",
-                    "Flowers, Cakes",
-                    "Hazratganj Lucknow Uttar Pradesh 226001",
-                    "5.0",
-                    "close"),
-                createList(
-                    "assets/LaPinzo.png",
-                    "La Pino'z Pizza (Hazratganj)",
-                    "Pizza, Italian",
-                    "Sapru Marg Lucknow UP 226001",
-                    "4.1",
-                    "open"),
-                createList(
-                    "assets/Mubeens.jpeg",
-                    "Mubeens",
-                    "Mughlai, Biryani, Kulcha Nehari",
-                    "Akbari Gate,Chowk Lucknow UP 226003",
-                    "3.0",
-                    "close"),
-                createList(
-                    "assets/Shabaan.png",
-                    "Shabaan Tikka Biryani",
-                    "Mughlai, Biryani",
-                    "Chowk Lucknow UP 226003",
-                    "5.0",
-                    "close"),
-                createList(
-                    "assets/Subway.jpg",
-                    "Subway (Hazratganj)",
-                    "Healthy Food, Fast Food",
-                    "6,Sapru Marg,Hazratganj Lucknow UP 226010",
-                    "4.0",
-                    "open"),
-                createList(
-                    "assets/Tunday.png",
-                    "Tunday Kababi (Aliganj)",
-                    "Vegetarian, Mughlai, Kebabs, Biryani",
-                    " Kapoorthala,Aliganj,Opp Novelty Cinema Lucknow UP 226024",
-                    "3.4",
-                    "open"),
-              ],
+              children: restaurant.map((e) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext ctx) => DescriptionPage()),
+                    );
+                  },
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15, right: 15),
+                      child: Column(
+                        children: [
+                          Card(
+                            child: Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 10, top: 15),
+                                          child: ClipRRect(
+                                            child: Container(
+                                              child: Image.asset(
+                                                e.image,
+                                                width: 80,
+                                                height: 80,
+                                              ),
+                                            ),
+                                            borderRadius:
+                                                BorderRadius.circular(50),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Container(
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 10),
+                                              child: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  SizedBox(
+                                                    height: 20,
+                                                  ),
+                                                  Text(
+                                                    e.restaurantName,
+                                                    style:
+                                                        TextStyle(fontSize: 20),
+                                                  ),
+                                                  SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  Text(e.description),
+                                                  Text(e.location)
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 20, left: 15),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow[800],
+                                        ),
+                                        Text(
+                                          e.rating,
+                                          style: TextStyle(
+                                              color: Colors.yellow[800]),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 40),
+                                          child: Text("Min Order: 99"),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 80),
+                                          child: Container(
+                                            width: 40,
+                                            height: 20,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(5),
+                                                color: (e.activate == "open")
+                                                    ? Colors.green
+                                                    : Colors.red),
+                                            child:
+                                                Center(child: Text(e.activate)),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
             ),
           ),
         ),
@@ -187,110 +318,8 @@ class _DishesHomeState extends State<DishesHome> {
     );
   }
 
-  Widget createList(String str1, String str2, String str3, String str4,
-      String str5, String str6) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (BuildContext ctx) => DescriptionPage()),
-        );
-      },
-      child: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15),
-          child: Column(
-            children: [
-              Card(
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10, top: 15),
-                              child: ClipRRect(
-                                child: Container(
-                                  child: Image.asset(
-                                    str1,
-                                    width: 80,
-                                    height: 80,
-                                  ),
-                                ),
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      SizedBox(
-                                        height: 20,
-                                      ),
-                                      Text(
-                                        str2,
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
-                                      Text(str3),
-                                      Text(str4)
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20, left: 15),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: Colors.yellow[800],
-                            ),
-                            Text(
-                              str5,
-                              style: TextStyle(color: Colors.yellow[800]),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 40),
-                              child: Text("Min Order: 99"),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 80),
-                              child: Container(
-                                width: 40,
-                                height: 20,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: (str6 == "open")
-                                        ? Colors.green
-                                        : Colors.red),
-                                child: Center(child: Text(str6)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  // Widget createList(String str1, String str2, String str3, String str4,
+  //     String str5, String str6) {
+
+  // }
 }
---
+++ lib/restaurant.dart
@@ -0,0 +1,17 @@
+class RestaurantName {
+  String image = '';
+  String restaurantName = '';
+  String description = '';
+  String location = '';
+  String rating = '';
+  String activate = '';
+
+  RestaurantName({
+    required this.image,
+    required this.restaurantName,
+    required this.description,
+    required this.location,
+    required this.rating,
+    required this.activate,
+  });
+}
